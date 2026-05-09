d7f6a5e829259eb207491a8992f362f8e61958b1
Suteki <a3234979817@outlook.com>
2026-05-09 22:16:24 +0800

    添加USART寄存器和初地址定义

---
+++ drivers/usart.h
@@ -0,0 +1,21 @@
+#ifndef USART_H
+#define USART_H
+
+//定义USART寄存器
+struct USART_REG
+{
+    volatile unsigned int SR;
+    volatile unsigned int DR;
+    volatile unsigned int BRR;
+    volatile unsigned int CR1;
+    volatile unsigned int CR2;
+    volatile unsigned int CR3;
+    volatile unsigned int GTPR;
+}
+
+//定义USART初地址
+#define USART1 ((volatile struct USART_REG*) 0x40013800)
+#define USART2 ((volatile struct USART_REG*) 0x40004400)
+#define USART3 ((volatile struct USART_REG*) 0x40004800)
+
+#endif
