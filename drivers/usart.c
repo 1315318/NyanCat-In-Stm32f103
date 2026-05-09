25a3b13ee2d1c4032c07d73e5fb8bf92f04085ee
Suteki <a3234979817@outlook.com>
2026-05-09 22:43:29 +0800

    Work in progress: USART configuration

---
+++ drivers/usart.c
@@ -0,0 +1,17 @@
+#include "usart.h"
+
+//发送/接受的数据
+void dr_usart(volatile struct USART_REG* usart, unsigned char data)
+{
+    CLEAN_BIT((usart->DR), (0xFF << 0));
+    SET_BIT((usart->DR), (0xFF << 0));
+}
+
+//设置波特率，mant为USART分频器除法因子(USARTDIV)的整数部分，farc为USART分频器除法因子(USARTDIV)的小数部分
+void brr_usart(volatile struct USART_REG* usart, unsigned int mant, unsigned char frac)
+{
+    CLEAN_BIT((usart->BRR), (0xF << 0));
+    SET_BIT((usart->BRR), ((frac & 0xF) << 0));
+    CLEAN_BIT((usart->BRR), (0xFFF << 4));
+    SET_BIT((usart->BRR), ((mant & 0xFFF) << 4));
+}
